3a7427f2ab10b437ec08cac536baa08f9337cf22
ZaharPro <zahar7331@gmail.com>
2022-11-18 22:27:23 +0300

    add Signals and EnumerableExtensions

---
+++ DSP3/Model/EnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DSP3.Model
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<TResult> SelectWithIndex<T, TResult>(this IEnumerable<T> source, Func<int, T, TResult> selector)
+        {
+            int i = 0;
+            foreach (var item in source)
+            {
+                yield return selector(i, item);
+                i++;
+            }
+        }
+    }
+}
---
+++ DSP3/Model/Signals.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DSP3.Model
+{
+    public static class Signals
+    {
+        private static readonly double PI2 = Math.PI * 2;
+
+        public static double CalcSignal(double amplitude, double frequency, double phase, int i, int n)
+        {
+            return amplitude * Math.Sin(PI2 * frequency * i / n + phase);
+        }
+
+        public static IEnumerable<double> CalcHarmonicSignals(int n, double amplitude, double frequency, double phase)
+        {
+            return Enumerable.Range(0, n)
+                .Select(i => CalcSignal(amplitude, frequency, phase, i, n));
+        }
+
+        public static IEnumerable<double> CalcPolyharmonicSignals(int n, IEnumerable<(double Amplitude, double Frequency, double Phase)> harmonics)
+        {
+            return Enumerable.Range(0, n)
+                .Select(i => harmonics
+                    .Select(harmonic => CalcSignal(harmonic.Amplitude, harmonic.Frequency, harmonic.Phase, i, n))
+                    .Sum());
+        }
+
+        public static IEnumerable<double> CalcSineSpectrums(int harmonicsCount, IEnumerable<double> signals)
+        {
+            var n = signals.Count();
+            var num = 2 / n;
+            return Enumerable.Range(0, harmonicsCount)
+                .Select(j => signals
+                    .SelectWithIndex((i, v) => v * Math.Sin(PI2 * i * j / n))
+                    .Sum())
+                .Select(v => v * num);
+        }
+
+        public static IEnumerable<double> CalcCosineSpectrums(int harmonicsCount, IEnumerable<double> signals)
+        {
+            var n = signals.Count();
+            var num = 2 / n;
+            return Enumerable.Range(0, harmonicsCount)
+                .Select(j => signals
+                    .SelectWithIndex((i, v) => v * Math.Cos(PI2 * i * j / n))
+                    .Sum())
+                .Select(v => v * num);
+        }
+
+        public static IEnumerable<double> CalcAmplitudeSpectrums(IEnumerable<double> sineSpectrums, IEnumerable<double> cosineSpectrums)
+        {
+            return sineSpectrums.Zip(cosineSpectrums, (sin, cos) => Math.Sqrt(sin * sin + cos * cos));
+        }
+
+        public static IEnumerable<double> CalcPhaseSpectrums(IEnumerable<double> sineSpectrums, IEnumerable<double> cosineSpectrums)
+        {
+            return sineSpectrums.Zip(cosineSpectrums, (sin, cos) => Math.Atan(sin / cos));
+        }
+
+        public static IEnumerable<double> RestoreSignals(int n, IEnumerable<(double Amplitude, double Phase)> spectrums)
+        {
+            return Enumerable.Range(0, n)
+                .Select(i => spectrums
+                    .SelectWithIndex((j, t) => t.Amplitude * Math.Cos(PI2 * i * j / n - t.Phase))
+                    .Sum());
+        }
+
+        public static IEnumerable<double> RestoreSignals(int n, IEnumerable<double> amplitudeSpectrums)
+        {
+            return Enumerable.Range(0, n)
+                .Select(i => amplitudeSpectrums
+                    .SelectWithIndex((j, amplitude) => amplitude * Math.Cos(PI2 * i * j / n))
+                    .Sum());
+        }
+    }
+}
