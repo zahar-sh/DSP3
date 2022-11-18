54b25de71369cc7c9fa91b40295e0d3e54843e18
ZaharPro <zahar7331@gmail.com>
2022-11-18 16:23:38 +0300

    upload project

---
+++ DSP3/Command/DelegateCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace DSP3.Command
+{
+    public class DelegateCommand : ICommand
+    {
+        private static readonly Action<object> DoNothing = o => { };
+        private static readonly Predicate<object> AnyTrue = o => true;
+
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public DelegateCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute ?? DoNothing;
+            _canExecute = canExecute ?? AnyTrue;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute(parameter);
+
+        public void Execute(object parameter) => _execute(parameter);
+    }
+}
---
+++ DSP3/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace DSP3.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T field, T newValue, string propertyName)
+        {
+            if (Equals(field, newValue))
+            {
+                return false;
+            }
+            field = newValue;
+            NotifyPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
