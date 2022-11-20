a2b2fd8e1488a4f14730278f30f9dc81ead6e3a4
ZaharPro <zahar7331@gmail.com>
2022-11-20 22:04:31 +0300

    add SignalType switcher

--- DSP3/View/MainWindow.xaml.cs
+++ DSP3/View/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using DSP3.Model;
+using DSP3.ViewModel;
+using System.Windows;
 
 namespace DSP3
 {
@@ -10,6 +12,21 @@ namespace DSP3
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SignalTypeChanged(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is MainWindowViewModel viewModel)
+            {
+                if (harmonicRadioButton.IsChecked.GetValueOrDefault())
+                {
+                    viewModel.SignalType = SignalType.Harmonic;
+                } 
+                else if (polyharmonicRadioButton.IsChecked.GetValueOrDefault())
+                {
+                    viewModel.SignalType = SignalType.Polyharmonic;
+                }
+            }
         }
     }
 }
